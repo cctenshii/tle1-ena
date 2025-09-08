4ac3f87b4cb9e9ee703f03d2e0247ac8530cc13c
Sissi <SissiWang2003@gmail.com>
2025-09-08 13:45:57 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,32 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Home</title>
+</head>
+<body>
+<header>
+    <img src="images/Logo-ENA.jpg" alt="Logo ENA">
+    <h1>ENA</h1>
+    <div>
+        <a class="button" href="logout.php">Logout</a>
+    </div>
+</header>
+<main>
+    <section>
+
+    </section>
+</main>
+<footer>
+    <p>Copyright 2025 - Zhiwen en Sissi</p>
+</footer>
+</body>
+</html>
